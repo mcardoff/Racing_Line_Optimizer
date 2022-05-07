49f9023c347d579e58f61cd71e2a6d3ce0edcf4b
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-05-06 19:35:49 -0500

    Initial Commit

---
+++ Shared/Racing_Line_Optimizer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Racing_Line_Optimizer_AppApp.swift
+//  Shared
+//
+//  Created by Michael Cardiff on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Racing_Line_Optimizer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
