6e587a61a2f58ac0d983d23c01973a194dfdbf43
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-05-06 19:45:05 -0500

    Port TrackProblem

--- Shared/TrackProblem.swift
+++ Shared/TrackProblem.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+// use overwritten init in problem formulation
+class RacingLineProblem {
+    var constraint : RacingLineConstraints
+    var costFunction : timeCostFunction
+    var currentXValues : [Double]
+    var currentYValues : [Double]
+    
+    var functionValue = 0.0
+    var squaredNorm = 0.0
+    var functionEvaluation = 0
+    var gradientEvaluation = 0
+    
+    init(costFunction : timeCostFunction, constraint : RacingLineConstraints, initialXValues : [Double], initialYValues : [Double]) {
+        self.costFunction = costFunction
+        self.constraint = constraint
+        self.currentXValues = initialXValues
+        self.currentYValues = initialYValues
+    }
+    
+    func reset() {
+        functionEvaluation = 0
+        gradientEvaluation = 0
+        functionValue = 0.0
+        squaredNorm = 0.0
+    }
+    
+    func value(xs: [Double], ys: [Double]) -> Double {
+        functionEvaluation += 1
+        return costFunction.costValue(xs: xs, ys: ys, constraint: constraint)
+    }
+}
